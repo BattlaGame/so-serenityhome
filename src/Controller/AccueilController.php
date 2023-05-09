ceb958d8c2fc74805794053502691d5e7e64bdb6
BattlaGame <115212531+BattlaGame@users.noreply.github.com>
2023-05-09 15:49:10 +0200

    Update AccueilController.php

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/accueil')]
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
